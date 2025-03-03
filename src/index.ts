3f00b99432d16a1b993d8009089830525456eb22
IvayloTsvetkov <ivaylo.icvetkov@gmail.com>
2025-03-03 10:54:38 +0200

    add cors and initial todos

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
+import cors from "cors";
+
+let i = 1;
+
+let todos = [
+  {
+    id: i++,
+    title: "Todo 1",
+    completed: true,
+  },
+  {
+    id: i++,
+    title: "Todo 2",
+    completed: false,
+  },
+  {
+    id: i++,
+    title: "Todo 3",
+    completed: false,
+  },
+];
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
