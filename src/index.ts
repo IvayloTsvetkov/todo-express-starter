fd517e804e073b2bc7f7dab3c998adb037038e02
IvayloTsvetkov <ivaylo.icvetkov@gmail.com>
2025-03-18 11:47:58 +0200

    add sqlite db

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,23 @@
 import express from "express";
 import cors from "cors";
+import Database from "better-sqlite3";
 
-let i = 1;
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
-let todos = [
-  {
-    id: i++,
-    title: "Todo 1",
-    completed: true,
-  },
-  {
-    id: i++,
-    title: "Todo 2",
-    completed: false,
-  },
-  {
-    id: i++,
-    title: "Todo 3",
-    completed: false,
-  },
-];
+const db = new Database("todos.db");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/todos", (req, res) => {
+  const todos = db.prepare<unknown[], Todo>("SELECT * from todos").all();
+  res.send(todos);
 });
 
 const port = 4000;
